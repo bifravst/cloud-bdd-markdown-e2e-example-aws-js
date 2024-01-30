1fabacb6782f8563074f870d0bdc3827670c033f
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-01-30 15:58:25 +0100

    Revert "chore: update husky to v9"
    This reverts commit 0c34cc05a96d01af5e9238e29033139eb3d8e0fc.

--- cdk/stackBaseName.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Used to globally define a "namespace" for this project's CloudFormation
- * stacks.
- */
-export const stackBaseName = (): string =>
-	process.env.STACK_BASE_NAME ?? 'e2e-bdd-test-runner-example'
--- features/markdown-reporter.ts
+++
@@ -1,5 +0,0 @@
-import { markdownReporter } from '@nordicsemiconductor/bdd-markdown'
-
-process.stdin.on('data', async (data) => {
-	console.log(await markdownReporter(JSON.parse(data.toString())))
-})
