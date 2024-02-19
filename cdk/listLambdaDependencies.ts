1fca0893e6e36ed89e4ebb8e84f134a99f025d6c
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-02-19 17:13:04 +0100

    style: migrate to @bifravst/eslint-config-typescript and @bifravst/prettier-config

--- cdk/listLambdaDependencies.ts
+++ cdk/listLambdaDependencies.ts
@@ -8,6 +8,6 @@ export const listLambdaDependencies = (entryFile: string): string[] => {
 		filename: entryFile,
 		directory: process.cwd(),
 		tsConfig: path.join(process.cwd(), 'tsconfig.json'),
-		filter: (path) => !path.includes('node_modules'), // do not include node_modules
+		filter: (path: string) => !path.includes('node_modules'), // do not include node_modules
 	})
 }
